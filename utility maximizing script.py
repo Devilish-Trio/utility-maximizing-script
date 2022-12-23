290ce8fb2badc28a66ea82d0a6b527b5f4408e36
dismay <93299449+Devilish-Trio@users.noreply.github.com>
2022-12-23 19:08:32 +1000

    Update utility maximizing script.py
    Made it more concrete, following the rules from here: https://courses.lumenlearning.com/wm-microeconomics/chapter/rules-for-maximizing-utility/

--- utility maximizing script.py
+++ utility maximizing script.py
@@ -1,29 +1,38 @@
-# Utility maximizing formula: (utility of item) / (cost of item)
+def maximize_utility(items, budget):
+  # Initialize total utility to 0
+  total_utility = 0
 
-# Function to calculate the utility maximizing formula
-def calc_util_maximizing_formula(utility, cost):
-  return utility / cost
+  # Sort the items by utility-to-cost ratio
+  items.sort(key=lambda x: x['utility'] / x['cost'], reverse=True)
 
-# Function to determine the best decision for purchasing items
-def determine_best_decision(items):
+  # Iterate through the items and add as much as we can afford
   best_item = None
-  max_util_per_cost = 0
-
-  for item in items:
-    util_per_cost = calc_util_maximizing_formula(item['utility'], item['cost'])
-    if util_per_cost > max_util_per_cost:
-      max_util_per_cost = util_per_cost
+  for i, item in enumerate(items):
+    if budget >= item['cost']:
+      budget -= item['cost']
+      total_utility += item['utility']
       best_item = item
+    else:
+      break
 
-  return best_item
+  # Output the rankings in plain English
+  rankings = {0: "#1", 1: "#2", 2: "#3"}
+  for i, item in enumerate(items):
+    ranking = rankings.get(i, f"{i+1}th")
+    print(f"{item['name']} with {item['utility'] / item['cost']:.3f} is the {ranking} best option")
 
-# (name) Want - (Item)enjoyment/need/quantity - (cost)cost/time
+  if best_item:
+    print(f"The best item to purchase is {best_item['name']} with a utility of {best_item['utility']} and a cost of {best_item['cost']}")
+
+  return total_utility
+
+# Test the function with some sample data
 items = [
-
   {'name': 'Item1', 'utility': 10, 'cost': 80},
   {'name': 'Item2', 'utility': 9, 'cost': 70},
   {'name': 'Item3', 'utility': 6, 'cost': 40},
+  {'name': 'Item4', 'utility': 5, 'cost': 350},
 ]
+budget = 150
 
-best_item = determine_best_decision(items)
-print(f'The best item to purchase is {best_item["name"]} with a utility of {best_item["utility"]} and a cost of {best_item["cost"]}')
+print(maximize_utility(items, budget))  # Expected output: 15 (1 Item1 + 1 Item2)
